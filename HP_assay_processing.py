18dad87cfb5a4f3c6b01675a268b77949fd49e5c
palendin <109877123+palendin@users.noreply.github.com>
2023-08-09 22:01:47 -0700

    Add files via upload

---
+++ HP_assay_processing.py
@@ -0,0 +1,142 @@
+# convert matrix (96 well plate format) into dataframe
+import pandas as pd
+import os
+from calculation import hydroxyproline_assay_calc
+import os, sys
+import warnings
+import mmap
+from tkinter import messagebox
+import time
+warnings.filterwarnings("ignore")
+
+# for executable purposes, defining a dynamic base path is needed for executing at different locations
+# for relative path, this is a folder created in the same directory as the executable for storing HP_assay experiments
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except:
+        base_path = os.path.abspath(".")
+
+    path = os.path.join(base_path, relative_path)
+
+    return path
+    
+def processing(root_directory):
+    # Loop through file in directory 
+    for folder_name in os.listdir(root_directory):
+        folder_path = os.path.join(root_directory, folder_name)
+        # check if subfolder
+        if os.path.isdir(folder_path):
+            print(f"Folder: {folder_path}")
+
+            # # Print the file names in the current subfolder
+            # for file_name in os.listdir(folder_path):
+            #     print(f" - {file_name}")
+
+            # have a text file to indicate which folder has been processed
+            remembered_folders_file = 'processed_files.txt'
+            with open(remembered_folders_file, 'r+') as f:
+                if folder_path not in f.read():
+                    f.write(folder_path + ',')     
+                    pass
+                else:
+                    continue
+
+            try:
+            # # Print the file names in the current subfolder
+            # for file_name in os.listdir(folder_path):
+            #     print(f" - {file_name}")
+
+                sample_layout = pd.read_excel(folder_path+'/' + 'sample_layout.xlsx',engine='openpyxl',sheet_name=None, skiprows=0) #skip the first row which is the indexes of the well
+                measurements = pd.read_excel(folder_path+'/' + 'abs_reading.xlsx',engine='openpyxl',sheet_name=None, skiprows=0) #skip the first row which is the indexes of the well
+
+            except:
+                continue
+            
+            # ------------------- format sample layout ----------------------------
+            combined_layout = pd.DataFrame()
+            layout_df_list = []
+            for sheet_name in sample_layout:
+                if sheet_name != 'Samples':
+                    # Column names
+                    column_names = ['ID', 'experiment_ID', 'sample_ID','sample_type','pilot_batch','tissue','hide_ID','hide_replicate',
+                                    'avg loaded weight mg','avg empty weight mg','net weight mg','operator', 'ug/well', 'media_type', 'scaffold_type', 'date']
+                    
+                    # drop the rows/columns and expand into appropriate dataframe
+                    df = sample_layout[sheet_name].iloc[0:,1:].reset_index(level=0,drop=True)
+                    df = df.melt()['value'].str.split(',',expand=True)
+
+                    # combine dataframes into single dataframe
+                    combined_layout = combined_layout.append(df)
+
+                    # put dataframes into a list
+                    layout_df_list.append(df)
+
+                    # combine dataframes with single row of header
+                    layout_df = pd.concat(layout_df_list, ignore_index=True)[combined_layout.columns]
+            
+            # replace header
+            for i, col_name in enumerate(layout_df.columns):
+                layout_df.rename(columns = {col_name: column_names[i]}, inplace = True)
+
+            # add constants to table
+            layout_df['homogenate volume ul'] = 500
+            layout_df['homogenate sample volume ul'] = 100
+            layout_df['digest volume ul'] = 1000
+            layout_df['digest sample volume ul'] = 20
+            
+            #  # ------------------- format absorbance measurement layout ----------------------------
+            # empty dataframe for combining multiple sheets together    
+            combined_abs = pd.DataFrame()
+            abs_df_list = []
+
+            # loop through each sheet in excel file
+            for sheet_name in measurements:
+                if sheet_name != 'General':
+                    #df = measurements[sheet_name].iloc[:,1:]
+                    df = measurements[sheet_name].iloc[0:,1:]
+                    df = df.values
+                    row = ['A', 'B', 'C', 'D','E','F','G','H']
+                    column = ['1','2','3','4','5','6','7','8','9','10','11','12']
+
+                    # Convert matrices into DataFrames with row and column names
+                    df_converted = pd.DataFrame(df, index=row, columns=column)
+
+                    # stack or melt dataframe
+                    #df_converted = pd.DataFrame(df, index=row, columns=column).stack().reset_index(name='abs').rename(columns={'level_0':'row','level_1':'column'})
+                    df_converted = df_converted.reset_index().melt(id_vars='index', var_name='column_name', value_name='abs')
+
+                    # Add sheet_name column to each DataFrame
+                    df_converted['sheet_name'] = sheet_name
+                    combined_abs = combined_abs.append(df_converted)
+
+                    # put dataframes into a list
+                    abs_df_list.append(df_converted)
+
+            # combine multiple dataframe and use single list of column names
+            combined_abs_df = pd.concat(abs_df_list, ignore_index=True)[df_converted.columns]
+            
+            # combined = combined.reset_index(level=0, drop=True)
+            combined_abs_df['location'] = combined_abs_df['index'] + combined_abs_df['column_name']
+            combined_abs_df = combined_abs_df.iloc[:,2:]
+            
+            # join layout and abs measurements together
+            final_layout = pd.concat([layout_df, combined_abs_df], axis=1)
+            #final_layout.to_csv('data.csv')
+
+            # run calculation
+            standard, samples, biopsy_results = hydroxyproline_assay_calc(final_layout)
+            #print(biopsy_results)
+        
+
+            # save the standard and samples only dataframes to csv to respective folder
+            #combined.to_csv(folder_path + '/' + 'test.csv')
+            samples.to_csv(folder_path + '/' + 'sample.csv')
+            standard.to_csv(folder_path + '/' + 'standard.csv')
+            biopsy_results.to_csv(folder_path + '/' + 'biopsy_result.csv')
+
+# if __name__ == "__main__":
+#     path = resource_path('HP_assay')
+#     processing(path)
+
+
---
+++ calculation.py
@@ -0,0 +1,90 @@
+# analysis for each run
+# make sure data table has correct label samples: standard, blank, sample, empty
+
+import pandas as pd
+import numpy as  np
+from sklearn.linear_model import LinearRegression
+import sklearn
+from matplotlib import pyplot as plt
+from sklearn.metrics import r2_score
+import os
+
+def hydroxyproline_assay_calc(df):
+
+    # normalize reading
+    blank_abs = df[df['sample_type'] == 'blank']['abs'].mean()
+    df['normalized_abs'] = df['abs'] - blank_abs
+
+    # tables of standards only (raw data)
+    standard = df[df['sample_type'] == 'standard']
+
+    model = LinearRegression()
+    model.fit(standard[['normalized_abs']], standard[['ug/well']])
+    r_squared = model.score(standard[['normalized_abs']], standard[['ug/well']])
+    slope = np.asscalar(np.array(model.coef_))
+    intercept = np.asscalar(np.array(model.intercept_))
+    #print(slope, intercept, r_squared)
+
+    # populate r_squared value in table of standards
+    standard['r_squared'] = r_squared
+    standard = standard.reset_index(level=0, drop=True)
+
+    # plot scatter vs predicted
+    #print(standard[['normalized_abs']], standard[['ug/well']])
+    #plt.scatter(standard[['normalized_abs']], standard[['ug/well']], color='g')
+    # plt.plot(standard[['normalized_abs']], model.predict(standard[['normalized_abs']]),color='k')
+    # plt.show()
+
+
+    # table of samples only (raw data)
+    samples = df[df['sample_type'] == 'sample']
+
+    # calculate 
+    samples['ug/well']= samples['normalized_abs']*slope+intercept
+    samples['mg/biopsy'] = samples['ug/well']/1000*(samples['homogenate volume ul']/samples['homogenate sample volume ul'])*(samples['digest volume ul']/samples['digest sample volume ul'])
+
+    # average results per hide_ID replicates (each hide_ID has 4 rep)
+    average_per_sample = samples.groupby(['experiment_ID','hide_ID', 'hide_replicate','scaffold_type'], squeeze=True).apply(lambda x: ((x['mg/biopsy'].sum()- x['mg/biopsy'].max()-x['mg/biopsy'].min())/2)).reset_index(name='mg/biopsy')
+    #print(average_per_sample)
+
+    # average mg/biopsy of the same sample_IDs, and standard dev
+    biopsy_mean = average_per_sample.groupby(['experiment_ID','hide_ID','scaffold_type'], squeeze = True).apply(lambda x: x['mg/biopsy'].mean()).reset_index(name='mg/biopsy mean')
+    biopsy_std = average_per_sample.groupby(['experiment_ID','hide_ID','scaffold_type'], squeeze = True).apply(lambda x: x['mg/biopsy'].std()).reset_index(name='mg/biopsy std')
+    
+    # convert to numeric before averaging. because sometimes it has type error
+    x = pd.to_numeric(samples['net weight mg'])
+    samples['x'] = x
+    biopsy_weight = samples.groupby(['hide_ID','scaffold_type'], squeeze = True).apply(lambda x: x['x'].mean()).reset_index(name='net weight mg')
+    
+    # add to biopsy_mean dataframe
+    biopsy_mean['mg/biopsy std'] = biopsy_std['mg/biopsy std']
+    biopsy_mean['net weight mg'] = biopsy_weight['net weight mg']
+    
+    # define scaffold weight base on their type
+    biopsy_mean.loc[biopsy_mean['scaffold_type'] == 'V2P1', 'scaffold weight mg'] = 5.75
+    biopsy_mean.loc[biopsy_mean['scaffold_type'] == 'PLA', 'scaffold weight mg'] = 4.6
+    
+    # define average % hydroxyproline in collagens
+    biopsy_mean['% hydroxyproline_in_collagen'] = 10.5
+    biopsy_mean['% percent_collagen'] = (biopsy_mean['mg/biopsy mean']/(biopsy_mean['% hydroxyproline_in_collagen']/100))/(biopsy_mean['net weight mg']-biopsy_mean['scaffold weight mg'])*100
+    #print(biopsy_mean)
+
+    # plt.bar(biopsy_mean['hide_ID'],biopsy_mean['mg/biopsy mean'])
+    # plt.errorbar(biopsy_mean['hide_ID'], biopsy_mean['mg/biopsy mean'], yerr=biopsy_mean['mg/biopsy std'], fmt='o', color ='r')
+    # plt.show()
+#print(average_abs_per_sample)
+    
+    return standard, samples, biopsy_mean
+
+if __name__ == "__main__":
+    df = pd.read_csv('test_HP_calc.csv')
+    standard, samples, biopsy_results = hydroxyproline_assay_calc(df)
+#print(average_per_sample)
+
+    # save CSV for standard, sample, and average result tables
+    # standard.to_csv('standard'+f'{count}.csv')
+    # samples.to_csv('samples'+f'{count}.csv')
+    # average_abs_per_sample.to_csv('average_abs_reading'+f'{count}.csv')
+
+    # upload to opvia and insert into DB (insert whole file or use API)
+
